321cc4bc0502483d98a6cb8f0b5503069d58c25c
Philippe Ds <phil.deiss@gmail.com>
2025-07-22 15:18:23 +0900

    Adding product deleting options

--- my_project_directory/src/Controller/ProductController.php
+++ my_project_directory/src/Controller/ProductController.php
@@ -36,4 +36,21 @@ final class ProductController extends AbstractController
             'createProductFrom' => $form,
         ]);
     }
+
+    #[Route('/product/{id}', name: 'app_product_delete', methods: ['POST'])] // POST because of html forms
+    public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    {
+        $submittedToken = $request->request->get('_token');
+
+        if ($this->isCsrfTokenValid('delete-product-' . $product->getId(), $submittedToken)) {
+            $entityManager->remove($product);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Product deleted successfully.');
+        } else {
+            $this->addFlash('error', 'Invalid CSRF token.');
+        }
+
+        return $this->redirectToRoute('app_account');
+    }
 }
